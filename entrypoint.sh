945900ff44312b643231d6fbf5cdfe26d3221555
PersonalValentinPirvulescu <86023799+PersonalValentinPirvulescu@users.noreply.github.com>
2021-06-28 15:50:38 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,7 +55,7 @@ fi
 echo "Copy contents to target git repository"
 #cp -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR"
 #mv "$CLONE_DIR/.git" "$TARGET_DIR"
-rsync -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR" --exclude '.git
+rsync -ra "$SOURCE_DIRECTORY"/. "$TARGET_DIR" --exclude '.git'
 cd "$TARGET_DIR"
 
 echo "Files that will be pushed:"
